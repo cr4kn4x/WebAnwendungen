7c8acb33a28082ad564e5e326529b4ac8c3829f5
cr4kn4x <s.heck0807@gmail.com>
2021-04-11 20:11:27 +0200

    Left_container fixed --> JS !

---
+++ Shop/app.js
@@ -0,0 +1,14 @@
+
+
+
+
+function adjustLeftContainerHeight(){
+	var height_shop_container;
+	var shop_container = document.getElementById('shop_container');
+	var left_container = document.getElementById('l_container');
+	height_shop_container = shop_container.offsetHeight;
+	left_container.style.height = height_shop_container;
+}
+
+
+adjustLeftContainerHeight();
