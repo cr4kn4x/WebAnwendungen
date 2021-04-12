4a82ba041da2f064f37967cae4fc772cd20518c0
maxi <maxi.geiger13@outlook.de>
2021-04-12 11:18:20 +0200

    Slider eingefügt

--- Shop-1/app.js
+++ Shop-1/app.js
@@ -4,8 +4,37 @@ var output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  output.innerHTML = this.value + " Euro";
 }
+
+slider.onload = function(){
+    output.innerHTML = this.value + " Euro"
+}
+
+
+
+var elements1 = document.getElementsByClassName("elements");;
+
+
+function show(){
+    
+    console.log(elements1[0]);
+
+    if(elements1[0].style.display == "none"){
+        for(let x=0; x<=8; x++){
+            elements1[x].style.display = "block-inline";
+        } 
+    }
+
+    else{
+        for(let x=0; x<=8; x++){
+            elements1[x].style.display = "none";
+        }
+    }
+        
+
+
+
 
 
 
@@ -25,31 +54,7 @@ adjustLeftContainerHeight();
 document.getElementsByID("shop_container").onresize = function() {adjustLeftContainerHeight()};
 
 
-var elements1;
 
-
-onload = (event) =>{   // wird aufgerufen sobald Webseite geladen wird
-    elements1 = document.getElementsByClassName("elements");
-
-}
-
-
-function show(){
-    
-    console.log(elements1[0]);
-
-    if(elements1[0].style.display == "none"){
-        for(let x=0; x<=8; x++){
-            elements1[x].style.display = "block-inline";
-        } 
-    }
-
-    else{
-        for(let x=0; x<=8; x++){
-            elements1[x].style.display = "none";
-        }
-    }
-        
 
 
     
