5f3aa23381f250896f0722d597f0499466127c2c
maxi <maxi.geiger13@outlook.de>
2021-04-12 15:16:06 +0200

    Layout angepasst

--- Shop-1/app.js
+++ Shop-1/app.js
@@ -32,28 +32,33 @@ function show(){
         
 
 
+    function function_plus(){
+        output.innerHTML = slider.value + 1;
+    }
 
-
-
-
-function adjustLeftContainerHeight(){
-	var height_shop_container;
-	var shop_container = document.getElementById('shop_container');
-	var left_container = document.getElementById('l_container');
-	height_shop_container = shop_container.offsetHeight;
-	left_container.style.height = height_shop_container;
-}
-
-
-
-
-adjustLeftContainerHeight();
-
-document.getElementsByID("shop_container").onresize = function() {adjustLeftContainerHeight()};
 
 
 
 
 
     
+    
+
+
+
 }
+
+
+
+function adjustLeftContainerHeight(){
+       var height_shop_container;
+        var shop_container = document.getElementById('shop_container');
+        var left_container = document.getElementById('l_container');
+        height_shop_container = shop_container.offsetHeight;
+        left_container.style.height = height_shop_container;
+    }
+    
+    
+    adjustLeftContainerHeight();
+    window.addEventListener("resize", adjustLeftContainerHeight);
+    
