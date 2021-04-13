6c4d010214afb099d89ad18b417d8726b6b516f7
maxi <maxi.geiger13@outlook.de>
2021-04-13 09:51:28 +0200

    neueste Datei

--- Shop/app.js
+++ Shop/app.js
@@ -1,10 +1,17 @@
-var elements1;
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
 
-onload = (event) =>{   // wird aufgerufen sobald Webseite geladen wird
-    elements1 = document.getElementsByClassName("elements");
-
+slider.oninput = function() {
+  output.innerHTML = this.value;
 }
+
+
+
+
+
+var elements1 = document.getElementsByClassName("elements");;
 
 
 function show(){
@@ -22,8 +29,57 @@ function show(){
             elements1[x].style.display = "none";
         }
     }
-        
 
-
-    
 }
+
+
+
+function function_plus(){
+    console.log(slider.value);
+    
+    slider.value++;
+    output.innerHTML = slider.value;
+}
+
+function function_minus(){
+    console.log(slider.value);
+    
+    slider.value--;
+    output.innerHTML = slider.value;
+}
+
+
+
+
+function adjustContainerHeight(){
+		var height_shop_container;
+		var height_left_container;
+        var shop_container = document.getElementById('shop_container');
+        var left_container = document.getElementById('l_container');
+		
+        height_shop_container = shop_container.offsetHeight;
+		
+		
+		height_left_container = left_container.offsetHeight;
+		
+		console.log("1:")
+		console.log(height_shop_container)
+		
+		console.log("2:")
+		console.log(height_left_container)
+		
+		if(height_left_container > height_shop_container){
+			shop_container.style.height = height_left_container+4;
+		}
+		
+		else{
+			left_container.style.height = height_shop_container;
+		}
+		
+        
+}
+    
+    
+adjustContainerHeight();
+window.addEventListener("resize", adjustContainerHeight);
+    
