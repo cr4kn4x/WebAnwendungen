1829853e9a1b58d291984206e8c6ad286afc06c4
cr4kn4x <81361445+cr4kn4x@users.noreply.github.com>
2021-06-25 14:36:55 +0200

    Update testFunction.test.js

--- Backend/testFunction.test.js
+++ Backend/testFunction.test.js
@@ -1,7 +1,7 @@
-const functions = require('./testFunction');
+const functions = require('./Test_Klasse.js');
 
 
 test('Add num1=2 to num2=2', () => {
-    const functions = new TestFunction();
+    const functions = new MyClass();
     expect(functions.add(2,2)).toBe(4);
 });
