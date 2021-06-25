dfe3aa13cda7c43c70847a27108ab69aaa037399
cr4kn4x <81361445+cr4kn4x@users.noreply.github.com>
2021-06-25 15:19:22 +0200

    Update Test_Klasse.js

--- Backend/Test_Klasse.js
+++ Backend/Test_Klasse.js
@@ -1,18 +1,5 @@
-class MyClass {
-  
-  methodOne() {
-    return 1;
-  }
-  
-  methodTwo() {
-    return 2;
-  }
-  
-  add(num1,num2){
-    return num1+num2;
-  }
-  
-  
-  
+function add(num1,num2){
+  return num1+num2; 
 }
-module.exports = MyClass;
+
+module.exports = add;
