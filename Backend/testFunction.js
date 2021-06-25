a609a87f688195c6a8ef1cc708a9d830052b6ff8
cr4kn4x <81361445+cr4kn4x@users.noreply.github.com>
2021-06-25 14:29:48 +0200

    Update testFunction.js

--- Backend/testFunction.js
+++ Backend/testFunction.js
@@ -1,4 +1,4 @@
-class TestFunctions{
+class TestFunction{
   
   add(num1, num2){
     return num1+num2;
@@ -7,5 +7,5 @@ class TestFunctions{
 
 }
 
-
-module.exports = TestFunctions;
+https://github.com/cr4kn4x/WebAnwendungen/blob/master/Backend/testFunction.js
+module.exports = TestFunction;
