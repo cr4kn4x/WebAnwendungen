5a283eab07d545068ab3b254f26f136f8de7c72a
cr4kn4x <81361445+cr4kn4x@users.noreply.github.com>
2021-06-25 15:25:29 +0200

    Update Test_Klasse.test.js

--- Backend/Test_Klasse.test.js
+++ Backend/Test_Klasse.test.js
@@ -1,6 +1,17 @@
-const functions = require('./Test_Klasse');
+const { add, mul, sub, div } = require('./arith');
 
+test('2 + 3 = 5', () => {
+  expect(add(2, 3)).toBe(5);
+});
 
-test('Add num1=2 to num2=2', () => {
-    expect(add(2,2)).toBe(4);
+test('3 * 4 = 12', () => {
+  expect(mul(3, 4)).toBe(12);
+});
+
+test('5 - 6 = -1', () => {
+  expect(sub(5, 6)).toBe(-1);
+});
+
+test('8 / 4 = 2', () => {
+  expect(div(8, 4)).toBe(2);
 });
